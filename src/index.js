642e4b2474595bd4e79211627d6cf95771eeaffd
kemalamru <kemalamru@gmail.com>
2024-08-22 17:18:18 +0700

    feat(router): add routing to other pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Page1 } from "./pages/page1";
+import { Page2 } from "./pages/page2";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/page2.jsx
@@ -0,0 +1,8 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>Page 2</h1>
+      <p>This is Page 2</p>
+    </div>
+  );
+};
