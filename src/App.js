226d42828896ffff3e1eff32d673eeb4a81ee805
kemalamru <kemalamru@gmail.com>
2024-08-29 15:34:12 +0700

    feat: coba ci cd

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Staging Environment</h1>
+        <h2>Coba CI/CD</h2>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
